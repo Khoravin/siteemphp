6d2d78921f1a4e68606b918006f0993e516b0bba
Khoravin <108277056+Khoravin@users.noreply.github.com>
2022-06-27 09:32:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalhando com php</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        /*$nota1 = 10;
+        $nota2 = 5;
+        $media = ($nota1+$nota2)/2;
+        if($media>=6){
+            echo 'O aluno foi aprovado';
+        }
+    else{
+        echo 'o aluno foi reprovado';
+    }*/
+
+    /*$a = 10;
+    if($a%2 == 0){
+        echo 'o número é par';
+    }else{
+        echo 'o número é impar';
+    }*/
+
+   
+
+    ?>
+
+</body>
+</html>
